e15f1de2027042977cbe50e9a31a6ec58cf085df
ixEkaterina <egorowa.ekaterina@gmail.com>
2021-10-31 00:19:31 +0300

    Challenge0

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "Challenge0"
+
+version := "0.1"
+
+scalaVersion := "2.12.15"
+
+idePackagePrefix := Some("ru.example")
+
+mainClass := Some("ru.example.Main")
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,27 @@
+package ru.example
+import scala.collection.mutable
+import scala.io.StdIn.readLine
+
+object Main extends App {
+  println("Please enter count of elements:")
+  val count = readLine().toInt
+
+  println("Please enter elements of collection:")
+  val strElem = readLine()
+
+// Easy )
+//  val arr = strElem.split(",")
+//  val realCount = arr.length
+//  val uniqCount = arr.toSet.size
+//
+//  println(realCount - uniqCount)
+
+// Imperative way
+  var i:Int = 0
+  val dict = mutable.Map[String, Int]()
+  strElem.split(",").foreach{x =>
+    if (dict.contains(x)) i = i + 1
+    else dict.put(x, 0)
+  }
+  println(i)
+}
