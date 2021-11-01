a4e72310fdadcb95a23662b2ba28d5dc7b072132
ixEkaterina <egorowa.ekaterina@gmail.com>
2021-11-01 15:56:45 +0300

    Delete println

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,10 +3,10 @@ import scala.collection.mutable
 import scala.io.StdIn.readLine
 
 object Main extends App {
-  println("Please enter count of elements:")
+//  println("Please enter count of elements:")
   val count = readLine().toInt
 
-  println("Please enter elements of collection:")
+//  println("Please enter elements of collection:")
   val strElem = readLine()
 
 // Easy )
